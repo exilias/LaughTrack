18a0f8f71af1c0f38a7dba57d106ca6f76e55b25
Shunya-Ueta <shunyaueta01@gmail.com>
2013-08-19 18:45:01 +0900

    近接センサーを使ってみた。

---
+++ Proximity Sensor/Proximity SensorTests/Proximity_SensorTests.h
@@ -0,0 +1,13 @@
+//
+//  Proximity_SensorTests.h
+//  Proximity SensorTests
+//
+//  Created by Ueta Shunya on 2013/08/19.
+//  Copyright (c) 2013年 Ueta Shunya. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Proximity_SensorTests : SenTestCase
+
+@end
---
+++ Proximity Sensor/Proximity SensorTests/Proximity_SensorTests.m
@@ -0,0 +1,32 @@
+//
+//  Proximity_SensorTests.m
+//  Proximity SensorTests
+//
+//  Created by Ueta Shunya on 2013/08/19.
+//  Copyright (c) 2013年 Ueta Shunya. All rights reserved.
+//
+
+#import "Proximity_SensorTests.h"
+
+@implementation Proximity_SensorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Proximity SensorTests");
+}
+
+@end
