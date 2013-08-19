e95cf69173d9f24562daec4dca2ce6566af235d6
Akira Ishii <exilias774@gmail.com>
2013-08-19 14:18:16 +0900

    プロジェクト作成

---
+++ LaughTrack/LaughTrack/LTViewController.h
@@ -0,0 +1,13 @@
+//
+//  LTViewController.h
+//  LaughTrack
+//
+//  Created by 石井 晃 on 13/08/19.
+//  Copyright (c) 2013年 OneChance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTViewController : UIViewController
+
+@end
